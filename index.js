31e9453368749e6fa4010bb8bf71c483d519ec91
mmrozewski <mmrozews@gmail.com>
2020-04-07 14:49:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+      return string[0].toUpperCase() + string.slice(1);
+    }
+
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+    }
+    
+    static titleize(string) {
+      let noNoWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+      Formatter.capitalize(string);
+      return (string.split(" ").map(word => { return (noNoWords.includes(word) && string.indexOf(word) != 0 ? word : Formatter.capitalize(word))})).join(" ")
+    }
 }
+
+
+
+
